9031cbb00e7c6be80afe41d103c1a61b1eb8128b
dqrtec <edaybrasil@gmail.com>
2019-12-12 15:47:02 -0300

    alter

--- cadim/src/main/java/br/arida/cadim/repositories/EcgRepository.java
+++
@@ -1,9 +0,0 @@
-package br.arida.cadim.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.arida.cadim.model.Ecg;
-
-public interface EcgRepository extends CrudRepository<Ecg, Integer> {
-
-}
